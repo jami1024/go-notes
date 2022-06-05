617fedfb83db115d353b2d0c9fec3c2d96279273
jami1024 <996013797@qq.com>
2022-06-05 16:36:37 +0800

    add 指针

--- point/point_statement_test.go
+++ point/point_statement_test.go
@@ -9,11 +9,22 @@ import (
 
 func TestPointStatement(t *testing.T) {
 	fmt.Println("指针的定义")
-	// 定义指针,初始值为nil
+	//定义⼀个变量
+	var a int = 2
+	fmt.Printf("变量a的地址为%p\n", &a) //通过%p占位符, &符号获取变量的内存地址。变量A的地址为0xc0000162d0
 
-	var username string = "zhangsan"
-	var name *string = &username // 取出username的地址赋值给name =》取引用
+	//创建⼀个指针
+	//指针的声明 通过 *T 表示T类型的指针
+	var i *int                  //int类型的指针
+	var f *float64              //float64类型的指针
+	fmt.Printf("指针i得值:%v\n", i) // < nil >空指针
+	fmt.Printf("指针f得值:%v\n", f) // < nil >空指针
 
-	// *name 取出name的内存地址中对应的存储的值 =》解引用
-	fmt.Printf("name的类型%T\tname的值%v\n", name, *name)
+	//因为指针存储的变量的地址 所以指针存储值
+	i = &a
+	fmt.Printf("i赋值后为:%v\n", i) //i存储a的内存地址0xc0000162d0
+	fmt.Println(*i)             //i存储这个指针存储的变量的数值2
+	*i = 100
+	fmt.Println(*i) //100
+	fmt.Println(a)  //100通过指针操作 直接操作的是指针所对应的数值
 }
